c3337f8cbaf1d4ba2b88a197b1ae221e17251b79
Remi LAK <remilak11@gmail.com>
2022-03-04 18:35:02 +0100

    meta

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'api.remilak.com'],
   },
   webpack(config) {
     config.module.rules.push({
